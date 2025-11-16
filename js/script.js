b9b163b8d5e95318332cb4817c4804103eec4198
Velki0 <b.gerardin125@gmail.com>
2025-11-16 17:43:29 +0100

    Révisions + Formulaire opérationnel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 // Gestion du formulaire de contact
+async function formulaire(prenomNom, email, sujet, message) {
+
+	const urlSubmitJSON = `https://api.submitjson.com/v1/submit/48p9LQRwX`;
+	const reponse = await fetch(urlSubmitJSON, {
+  		method: 'POST',
+  		headers: {
+    		'content-type': 'application/json',
+    		'X-API-Key': 'sjk_6ca5afa9b4ab42b09129774dce438bd0',
+  		},
+  		body: JSON.stringify({
+    		data: {
+      			prenomNom: prenomNom,
+      			email: email,
+      			sujet: sujet,
+	  			message: message
+   			}
+  		})
+	});
+
+	const submission = await reponse.json()
+	
+	console.log('📦 Submission response:', submission)
+	const confirmationEnvoi = document.getElementById("confirmationEnvoi");
+	confirmationEnvoi.style.opacity = "100%";
+}
+
 document.getElementById('formulaireContact').addEventListener('submit', (event) => { 
 	
 	event.preventDefault(); 
@@ -18,10 +44,11 @@ document.getElementById('formulaireContact').addEventListener('submit', (event) 
 	if (validationSujet() == null) { erreurSujet.style.opacity = "100%"; return; }
 	if (!message) { erreurMessage.style.opacity = "100%"; return; }
 	
-	confirmationEnvoi.style.opacity = "100%";
+	formulaire(prenomNom, email, sujet, message);
 
 	function validationPrenomNom() { return prenomNom.match(/^[a-zA-Z ]+$/); }
 	function validationEmail() { return email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/); };
 	function validationSujet() { return sujet.match(/^[\w\d ]+$/); };
 	
 });
+
